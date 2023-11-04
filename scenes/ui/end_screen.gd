c6eee180e043a32deb17da1a2d78304e7f7a0930
akex <hortinstein@gmail.com>
2023-11-04 12:07:14 -0400

    61/81 comp lete -added sounds

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -18,6 +18,13 @@ func _ready():
 func set_defeat():
 	$%TitleLabel.text = "Defeat"
 	$%DescriptionLabel.text = "You Lost!"
+	play_jingle(true)
+
+func play_jingle(defeat:bool = false):
+	if defeat:
+		$DefeatStreamPlayer.play()
+	else: 
+		$VictoryStreamPlayer.play()
 	
 
 func on_restart_button_pressed():
