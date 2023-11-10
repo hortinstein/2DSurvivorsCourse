43aaf916edc4d9f67fd9a82a90c74452914906e3
akex <hortinstein@gmail.com>
2023-11-10 13:04:09 -0500

    69/81 completed

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -1,25 +1,17 @@
 extends PanelContainer
 
-signal selected
 
 @onready var name_label: Label = $%NameLabel
 @onready var description_label: Label = $%DescriptionLabel
 
-var disabled = false
 
 func _ready():
 	gui_input.connect(on_gui_input)
-	mouse_entered.connect(on_mouse_entered)
-
-func play_in(delay: float= 0):
-	modulate = Color.TRANSPARENT
-	await get_tree().create_timer(delay).timeout
-	modulate = Color.WHITE
-	$AnimationPlayer.play("in")
-
+	
 
 func play_discard():
 	$AnimationPlayer.play("discard")
+
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
@@ -27,26 +19,9 @@ func set_ability_upgrade(upgrade: AbilityUpgrade):
 	
 
 func select_card():
-	disabled = true
 	$AnimationPlayer.play('selected')	
 	
-	for other_card in get_tree().get_nodes_in_group("upgrade_card"):
-		if other_card == self:
-			continue
-		other_card.play_discard()
-		
-	await $AnimationPlayer.animation_finished
-	selected.emit()
-		
 
 func on_gui_input (event: InputEvent):
-	if disabled: 
-		return 
-		
 	if event.is_action_pressed("left_click"):
 		select_card()
-
-func on_mouse_entered():
-	if disabled:
-		return
-	$HoverAnimationPlayer.play('hover')
